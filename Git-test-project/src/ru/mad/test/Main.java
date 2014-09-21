cfa6f4809078f6440af371d7351ac3a75438954b
MichK <MichK@likeaboss>
2014-09-21 23:46:32 +1000

    removed !!

--- Git-test-project/src/ru/mad/test/Main.java
+++ Git-test-project/src/ru/mad/test/Main.java
@@ -3,7 +3,7 @@ package ru.mad.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!!!!!!!!!11");
+		System.out.println("Hello from Git Hub!!!!!!!!!11");
 	}
 
 }
